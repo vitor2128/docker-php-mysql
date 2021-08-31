456b540783bb3f958ba14e9a768cbe6de7962251
Vitor Hugo <53013462+vitor2128@users.noreply.github.com>
2021-08-31 12:08:52 -0300

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,33 +1,9 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
   <head>
-    <meta charset="UTF-8">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title>Teste PHP</title>
   </head>
   <body>
-    <header class="cabecalho">
-      <h1>PHP</h1>
-    </header>
-    <main class="principal">
-      <div class="conteudo">
-        <nav class="modulos">
-          <div class="modulo verde">
-            <ul>
-              <li>
-                <a href="exercicio.php?dir=basico&file=ola">
-                  Olá PHP
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </main>
+    <p>Olá Mundo</p>
   </body>
 </html>
